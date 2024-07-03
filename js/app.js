83fda0c7619f0447d5c5fc1abac097591f6d78be
Okolo Uchenna <148909149+eduvibe@users.noreply.github.com>
2024-07-03 20:54:20 +0100

    Ajustment

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+const hamburger = document.getElementById('hamburger');
+const closeBtn = document.getElementById('close');
+const links = document.querySelector('.links');
+
+hamburger.addEventListener('click', () => {
+    links.classList.toggle('menu-active');
+    hamburger.style.display = 'none';
+    closeBtn.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    links.classList.toggle('menu-active');
+    hamburger.style.display = 'block';
+    closeBtn.style.display = 'none';
+});
+
 function updateTime() {
     const utcTimeElement = document.getElementById('utcTime');
     const currentDayElement = document.getElementById('currentDay');
